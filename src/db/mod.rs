798e579b702d0012322fc735bf4edc9d7ed44923
Aitthi Arsa <meanstack20@gmail.com>
2023-07-28 09:42:35 +0700

    feat: database namespace

--- src/db/mod.rs
+++ src/db/mod.rs
@@ -1,7 +1,7 @@
 // internal 
 pub mod model;
 
-use surrealdb::{Surreal, engine::local::Db};
+use surrealdb::{Surreal, engine::local::{Db, RocksDb, Mem}};
 use tokio::sync::OnceCell;
 
 pub struct Database {
@@ -12,8 +12,16 @@ pub struct Database {
 pub async fn get_database()  ->  &'static Database {
   static GLOBAL_DB: once_cell::sync::Lazy<OnceCell<Database>> = once_cell::sync::Lazy::new(OnceCell::new);
   let dbs = GLOBAL_DB.get_or_init(|| async {
-    let disk = surrealdb::Surreal::new::<surrealdb::engine::local::RocksDb>("easy_proxy.db").await.unwrap();
-    let memory = surrealdb::Surreal::new::<surrealdb::engine::local::Mem>(()).await.unwrap();
+    let disk = Surreal::new::<RocksDb>("easy_proxy.db").await.unwrap();
+    let memory = Surreal::new::<Mem>(()).await.unwrap();
+    match disk.use_ns("easy_proxy").use_db("easy_proxy").await {
+      Ok(_) => {},
+      Err(_) => {}
+    }
+    match memory.use_ns("easy_proxy").use_db("easy_proxy").await {
+      Ok(_) => {},
+      Err(_) => {}
+    }
     Database {
       disk,
       memory
--- src/db/model/admin.rs
+++ src/db/model/admin.rs
@@ -1,7 +1,10 @@
-use serde::Deserialize;
+use serde::{
+  Deserialize,
+  Serialize
+};
 use surrealdb::sql::Thing;
 
-#[derive(Clone, Debug, Deserialize)]
+#[derive(Clone, Debug, Deserialize, Serialize)]
 pub struct Admin {
   pub id: Thing,
   pub name: String,
