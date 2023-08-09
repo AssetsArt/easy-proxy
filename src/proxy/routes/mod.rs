f92f8fab71d82a31dbdc36d01f42ca2dfdee9ae9
Aitthi Arsa <meanstack20@gmail.com>
2023-08-10 01:22:07 +0700

    🚧 find_route

--- src/proxy/routes/mod.rs
+++ src/proxy/routes/mod.rs
@@ -2,14 +2,14 @@ use bytes::Bytes;
 use http::Request;
 use http_body_util::combinators::BoxBody;
 
-pub struct RequestInfo {
+pub struct RouteMeta {
   pub ip: String,
   pub port: u16,
   pub protocol: String,
 }
 
-pub async fn find_route(_req: &Request<BoxBody<Bytes, hyper::Error>>) -> RequestInfo {
-  RequestInfo {
+pub async fn find_route(_req: &Request<BoxBody<Bytes, hyper::Error>>) -> RouteMeta {
+  RouteMeta {
     ip: "127.0.0.1".to_string(),
     port: 3000,
     protocol: "http".to_string(),
