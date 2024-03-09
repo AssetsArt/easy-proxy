5b48bfb148cbd14ddc1bb76b1599351502722e94
Aitthi Arsa <meanstack20@gmail.com>
2024-03-09 12:48:11 +0700

    🎨 update runtime config

--- config/src/runtime.rs
+++ config/src/runtime.rs
@@ -59,32 +59,31 @@ pub struct ProviderFiles {
 
 // Initialize global configuration
 lazy_static! {
-    static ref GLOBAL_CONFIG: OnceCell<RuntimeConfig> = OnceCell::new();
+    static ref GLOBAL_RUNTIME_CONFIG: OnceCell<RuntimeConfig> = OnceCell::new();
 }
 
 pub fn initialize() -> Result<(), anyhow::Error> {
-    let conf_path = std::env::var("EASY_PROXY_CONF");
-    let conf_path = match conf_path {
+    let conf_path = match std::env::var("EASY_PROXY_CONF") {
         Ok(val) => val,
         Err(_e) => {
-            let conf_path = std::env::current_dir().expect("Unable to get current dir");
+            let conf_path = std::env::current_dir().map_err(|e| anyhow::anyhow!(e))?;
             conf_path
                 .join(".config/easy_proxy.yaml")
                 .to_str()
-                .expect("Unable to convert path")
+                .unwrap_or_default()
                 .to_string()
         }
     };
 
-    let open_conf = File::open(conf_path).expect("Unable to open file");
+    let open_conf = File::open(conf_path).map_err(|e| anyhow::anyhow!(e))?;
     let read_conf = BufReader::new(open_conf);
-    let conf: RuntimeConfig = serde_yaml::from_reader(read_conf).expect("Unable to read conf file");
-    GLOBAL_CONFIG
+    let conf: RuntimeConfig = serde_yaml::from_reader(read_conf).map_err(|e| anyhow::anyhow!(e))?;
+    GLOBAL_RUNTIME_CONFIG
         .set(conf)
         .map_err(|_| anyhow::anyhow!("Unable to set global config"))
 }
 
 pub fn config() -> &'static RuntimeConfig {
     // SAFETY: This is safe because we are initializing the global config
-    GLOBAL_CONFIG.get().expect("Config not initialized")
+    GLOBAL_RUNTIME_CONFIG.get().expect("Config not initialized")
 }
