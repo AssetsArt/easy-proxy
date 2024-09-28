0093d5216a35156ca9f60d31b6506e9985cf2a88
detoro <meanstack20@gmail.com>
2024-09-28 13:35:14 +0700

    ♻️ cleanup

--- src/config/runtime.rs
+++ src/config/runtime.rs
@@ -1,21 +1,21 @@
 use crate::errors::Errors;
-use lazy_static::lazy_static;
 use once_cell::sync::OnceCell;
-use std::{fs::File, io::BufReader};
+use serde::{Deserialize, Serialize};
+use std::{env, fs::File, io::BufReader, path::PathBuf};
 
-#[derive(Debug, Clone, serde::Deserialize, serde::Serialize)]
+#[derive(Debug, Clone, Deserialize, Serialize)]
 pub struct RuntimeConfig {
     pub proxy: Proxy,
     pub pingora: Pingora,
     // pub providers: Vec<Provider>,
 }
 
-#[derive(Debug, Clone, serde::Deserialize, serde::Serialize)]
+#[derive(Debug, Clone, Deserialize, Serialize)]
 pub struct Proxy {
     pub addr: String,
 }
 
-#[derive(Debug, Clone, serde::Deserialize, serde::Serialize)]
+#[derive(Debug, Clone, Deserialize, Serialize)]
 pub struct Pingora {
     pub daemon: Option<bool>,
     pub threads: Option<usize>,
@@ -29,36 +29,32 @@ pub struct Pingora {
 }
 
 // Initialize global configuration
-lazy_static! {
-    static ref GLOBAL_RUNTIME_CONFIG: OnceCell<RuntimeConfig> = OnceCell::new();
-}
+static GLOBAL_RUNTIME_CONFIG: OnceCell<RuntimeConfig> = OnceCell::new();
 
 pub fn initialize() -> Result<(), Errors> {
-    let conf_path = match std::env::var("EASY_PROXY_CONF") {
-        Ok(val) => val,
-        Err(_e) => {
-            let conf_path = std::env::current_dir().map_err(|e| {
-                Errors::ConfigError(format!("Unable to get current directory: {}", e))
-            })?;
-            conf_path
-                .join(".config/easy_proxy.yaml")
-                .to_str()
-                .unwrap_or_default()
-                .to_string()
-        }
+    let conf_path = if let Ok(val) = env::var("EASY_PROXY_CONF") {
+        PathBuf::from(val)
+    } else {
+        let mut conf_path = env::current_dir().map_err(|e| {
+            Errors::ConfigError(format!("Unable to get current directory: {}", e))
+        })?;
+        conf_path.push(".config/easy_proxy.yaml");
+        conf_path
     };
 
-    let open_conf = File::open(conf_path)
-        .map_err(|e| Errors::ConfigError(format!("Unable to open config file: {}", e)))?;
-    let read_conf = BufReader::new(open_conf);
-    let conf: RuntimeConfig = serde_yml::from_reader(read_conf)
-        .map_err(|e| Errors::ConfigError(format!("Unable to parse config file: {}", e)))?;
+    let file = File::open(&conf_path).map_err(|e| {
+        Errors::ConfigError(format!("Unable to open config file {:?}: {}", conf_path, e))
+    })?;
+    let reader = BufReader::new(file);
+    let config: RuntimeConfig = serde_yml::from_reader(reader).map_err(|e| {
+        Errors::ConfigError(format!("Unable to parse config file {:?}: {}", conf_path, e))
+    })?;
+
     GLOBAL_RUNTIME_CONFIG
-        .set(conf)
-        .map_err(|_| Errors::ConfigError("Unable to set global config".to_string()))
+        .set(config)
+        .map_err(|_| Errors::ConfigError("Global config has already been set".to_string()))
 }
 
 pub fn config() -> &'static RuntimeConfig {
-    // SAFETY: This is safe because we are initializing the global config
     GLOBAL_RUNTIME_CONFIG.get().expect("Config not initialized")
 }
